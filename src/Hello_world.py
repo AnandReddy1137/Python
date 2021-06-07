ef0c95cfae39eba152bdc6ba7e8741df32f2b9e8
“anandreddy1137” <akakarad@informatica.com>
2021-06-07 20:04:47 +0530

    Adding Identifiers.py

---
+++ src/Hello_world.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+
+sentence= ''' 
+------------------------------------
+Welcome to the world of Python!!!
+------------------------------------
+Features of Python:
+1. Simple and easy to learn
+2. Freeware and Open Source
+3. High Level Programming language
+4. Platform Independent
+5. Portability
+6. Dynamically Typed
+7. Both Procedure Oriented and Object Oriented
+8. Interpreted
+9. Extensible
+10. Embedded
+11. Extensive Library
+--------------------------------------
+'''
+
+print(sentence)
